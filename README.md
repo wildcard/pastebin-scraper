7146af386e2e25677e1f40d1fa23217f2b457d57
Kobi Kadosh <kobik@wix.com>
2022-09-16 19:33:46 +0300

    snippet page draft

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 16:44-18:00 | parsing tests
 ---
 18:00-18:30 | Dumping snippt page
+---
+19:00-19:30 | prototyping Dumping snippet page
+---
+| tests
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const PASTEBIN_BASE_URL = 'https://pastebin.com'
 const PASTEBIN_URL = `${PASTEBIN_BASE_URL}/archive`
 
-const pages = [];
+const sessions = [];
 const db = [];
 
 async function dumpIndexPage(ts, html) {
@@ -17,7 +17,17 @@ async function dumpSnippetPage(ts, id, html) {
     await fs.writeFile(`./dump/sessions/${ts}/${id}.htm`, html);
 }
 
-function parseItem($, ts, $tr) {
+function buildSnippetPath(ts, id) {
+   return `./dump/sessions/${ts}/${id}.dump.txt`; 
+}
+async function dumpSnippetText(ts, id, txt) {
+    await fs.writeFile(buildSnippetPath(ts, id), txt); 
+}
+
+async function dumpDb() {
+    await fs.writeFile('./dump/db.json', JSON.stringify(db, null, 2))
+}
+function parseItem($, $tr) {
     const $a = $('td:first > a', $tr);
     const $sytax = $('a', $('td').eq(2))
 
@@ -30,38 +40,57 @@ function parseItem($, ts, $tr) {
     }
 }
 
-function parseIndexPage(ts, html) {
+function parseIndexPage(html) {
     const $ = cheerio.load(html)
     const $tableRows = $('.archive-table .maintable tr');
     const items = [];
     $tableRows.each((i, $elm) => {
         if (i > 0) {
-            items.push(parseItem($, ts, $elm));
+            items.push(parseItem($, $elm));
         }
     })
 
-    // console.log(items);
     return items
 }
 
-function parseSnippetPage(ts, html) {
+function parseSnippetPage(html, { ts, id }) {
     const $ = cheerio.load(html);
+    const $title = $('.info-bar h1')
+
+    return {
+        title: $title.text(),
+        // TODO: author, Date
+        filePath: buildSnippetPath(ts, id)
+    }
 }
 
 async function scrapeSnippetPage(ts, link, { id }) {
     const res = await axios.get(`${PASTEBIN_BASE_URL}/${link}`) 
     await dumpSnippetPage(ts, res.data, id);
     const resDl = await axios.get(`${PASTEBIN_BASE_URL}/dl/${id}`)
-
+    await dumpSnippetText(ts, id, resDl.data)
+    return parseSnippetPage(res.data, { ts, id })
 }
 
 async function scrapeIndexPage() { 
     const res = await axios.get(PASTEBIN_URL)
     const ts = Date.now();
-    pages.push({ ts, page: res.data});
-
+    
     await dumpIndexPage(ts, res.data);    
-    parseIndexPage(ts, res.data);
+    const items = parseIndexPage(ts, res.data);
+    
+    await Promise.all(items.map(async (item) => {
+        const { link } = item;
+        const snippetData = await scrapeSnippetPage(ts, link, item);
+        
+        db.push({
+            ...item,
+            ...snippetData 
+        })
+    }))
+    
+    sessions.push({ ts, db });
+    await dumpDb()
 }
 
 scrapeIndexPage();
